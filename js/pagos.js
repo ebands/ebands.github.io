e13c5974d2742601d1cce1d1cd29631e56d6bfe3
thiago980709 <santixdim100@gmail.com>
2018-03-06 21:21:18 -0500

    cambios

--- js/pagos.js
+++ js/pagos.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     pop =  $("#pop-up-info");
-
+    date =    $("#date").datepicker()
     $("#btnAdd").click(function(){
         pop.bPopup({
             zIndex: 4,
@@ -8,7 +8,22 @@ $(document).ready(function(){
           position: [750, 150]
         });
     });
-
-    $("#date").datepicker();
+    $(".b-close").click(function(){
+        console.log("SDF")
+     date.datepicker("hide");
+    });
   
+<<<<<<< HEAD
 });
+=======
+ 
+});
+ 
+ 
+
+  
+   
+
+ 
+
+>>>>>>> 8ea83d501b0c4adfcd1b30b51910289646ee251f
