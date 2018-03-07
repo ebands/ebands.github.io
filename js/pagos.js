dee094367106d13f1ab9afc53097128ae2205ae9
andresarangopro <andresarangopro@gmail.com>
2018-03-06 20:47:45 -0500

    arreglo dataPicker

--- js/pagos.js
+++ js/pagos.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
+    pop =  $("#pop-up-info");
+
+    $("#btnAdd").click(function(){
+        pop.bPopup({
+            zIndex: 4,
+          modalClose: false,
+          position: [750, 150]
+        });
+    });
+
     $("#date").datepicker();
+  
 });
+ 
+ 
+
+  
+   
+
+ 
+
