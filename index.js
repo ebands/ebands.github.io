347bea12951626fa4d05664f6671c5a5f61eb269
andresarangopro <andresarangopro@gmail.com>
2018-05-14 21:41:01 -0500

    Init

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-var express = require('express')
-var PORT = process.env.PORT || 5000
+var express = require('express');
+var app = express();
 
-app = express()
+// set port 
+var port = process.env.PORT || 5000
 
-var route = express.Router();
+app.use(express.static(__dirname));
+
+app.get("/", function(req, res){
+    res.render("index");
+})
+
+app.listen(port, function(){
+    console.log("app running");
+})
+
+/*var route = express.Router();
 
 route.get('/', function(req, res){
     res.json({'mensaje':'Hola Api'})
@@ -11,4 +22,4 @@ route.get('/', function(req, res){
 
 //El use es para asignarle una ruta por defecto
 app.use('/api', route)
-app.listen(PORT, ()=> console.log('Escuchando en el puerto ', PORT))
+app.listen(PORT, ()=> console.log('Escuchando en el puerto ', PORT))*/
