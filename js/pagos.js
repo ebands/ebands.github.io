fccb70b86a862367aa336d983de94ea5201e0b78
andresarangopro <andresarangopro@gmail.com>
2018-05-16 01:53:39 -0500

    full app v3.

--- js/pagos.js
+++ js/pagos.js
@@ -4,6 +4,8 @@ var date;
 var mapPagos={};
 var tblPagoEst = $("#tblPagoEst").DataTable();
 var popPago =$("#pop-up-pago");
+var keyStudent;
+var keyPago;
 $(document).ready(function(){
     pop =  $("#pop-up-info");
     date =    $("#date").datepicker()
@@ -76,12 +78,32 @@ jumb.on('click', '.btn', function () {
     // var button = $(this).getAttribute('href');      
     popPago.bPopup({           
         zIndex: 4,
-        modalClose: false,
-        position: [310, 92]
+        modalClose: true,
+        position: [310, 76]
     });
-  
+    keyPago = array.attr("id");    
     getStudents();
 });
+
+tblPagoEst.on('change', '.checkbox', function () {     
+       
+    var RowIndex = $(this).closest('tr');
+    var data = tablePagos.row(RowIndex).data();    
+
+     var boxes = $('input[name='+mapKeyDoc[data[0]]+']:checked');     
+     boxes.each(function(){          
+         copyMapFecha[$(this).val()]=1;            
+        
+     });     
+     var unboxes =$('input[name='+mapKeyDoc[data[0]]+']:not(:checked)');
+     unboxes.each(function(){          
+         copyMapFecha[$(this).val()]=0;            
+       
+     });    
+     addPagoEstudent("a"+year,mapKeyDoc[data[0]], copyMapFecha);
+ 
+   });
+
 
 /**Metodo que vacía una lista de campos**/
 function vaciarCamps(){
@@ -92,26 +114,30 @@ function vaciarCamps(){
 }
 
   
-$("#b-closePago").click(function(){
+$("#b-closePago").click(function(){  
     popPago.close();
 });
 
 function getStudents(){
+    tblPagoEst
+     .clear()
+     .draw();
     $.ajax({
         url: "/read_pagosEst",
         type: 'GET',
         //dataType: 'json', // added data type
         success: function(res) {
           var btnCheck ="<input type='image' src='../img/checkPago.png'  class='edicion' />";          
-         
+        
           res.forEach(function(element) {
+            var checkbox1 = ' <input type="checkbox" name="'+ element[0]+'" value="enero" class="checkbox">';
             console.log(element);
             tblPagoEst.row.add([
               element[0],
               element[1],
               element[2],
               element[3],
-              btnCheck
+              checkbox1 
             
             ]).draw();  
           });    
