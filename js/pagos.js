33bd66e4bd49836a4b4f192ce75540eeece1e63b
thiago980709 <santixdim100@gmail.com>
2018-05-15 21:49:16 -0500

    creacion boton tipo pago

--- js/pagos.js
+++ js/pagos.js
@@ -1,3 +1,5 @@
+var inputPago= $("#namePagos");
+
 $(document).ready(function(){
     pop =  $("#pop-up-info");
     date =    $("#date").datepicker()
@@ -16,11 +18,89 @@ $(document).ready(function(){
     $("#btnAceptar").click(function(){
         pop.close();
         date.datepicker("hide");
+        postTipoPago();
     });
+
+
   
 
 });
 
+
+function postTipoPago(){
+    
+    var options = {
+      url: "/write_pago",
+      dataType: "text",
+      type: "POST",
+      data: {nameTipoPago:inputPago.val(),
+          fecha:date.val() },//test: JSON.stringify( data ) }, // Our valid JSON string
+      success: function( data, status, xhr ) {
+         //...
+      },
+      error: function( xhr, status, error ) {
+          //...
+      }
+    };
+    $.ajax( options );
+}
+
+
+function getData() { 
+    //userId = firebase.auth().currentUser.uid
+      return redt.once('value').then(function(snapshot) {  		
+      snapshot.forEach(function(child){       
+        var key = child.key;
+        var value = child.val();
+        var nameTipoPago = value.nameTipoPago;
+        var fecha = value.fecha;
+        var button = document.createElement('input');
+        button.type = 'button';
+        button.className = 'btn btn-primary btn-lg btn block'
+       
+  
+         
+      });
+    });
+  }
+
+
+function getFromServ(){
+ 
+    $.ajax({
+      url: "/read_pagos",
+      type: 'GET',
+      //dataType: 'json', // added data type
+      success: function(res) {
+        var button = "<input type='button' class='btn btn-primary btn-lg btn-block'/>"
+        res.forEach(function(element) {
+          console.log(element);
+          var button = document.createElement('input');
+          button.type = 'button';
+          button.className = 'btn btn-primary btn-lg btn block'
+          button.value = element[2]
+
+          
+          
+        });
+      
+    
+      },error: function (res, status, error) {
+        alert("si, error", error);
+      }
+  });
+  }
+  
+  function getF(){
+    $.getJSON( '/read', function( data ) {
+  
+      // For each item in our JSON, add a table row and cells to the content string
+      $.each(data, function(){
+        console.log(data)
+      });
+  
+    });
+  }
 
  
 
